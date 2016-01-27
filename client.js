10ac2628d2a55e6dbd9dc5dbdbad418ac336ebc7
awb99 <awb99@gmail.com>
2016-01-27 15:22:57 +0100

    port 33333

--- client.js
+++ client.js
@@ -2,13 +2,13 @@
 var zmq = require('zmq')
   , sock = zmq.socket('req');
  
-sock.connect('tcp://104.222.96.51:35000');
-console.log('Worker connected to port 35000');
+sock.connect('tcp://104.222.96.51:33333');
+console.log('Worker connected to port 33333');
 
 setInterval(function(){
   console.log('requesting data');
   sock.send('calendar');
-}, 500);
+}, 5000);
 
  
 sock.on('message', function(msg){
--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 var zmq = require('zmq')
   , sock = zmq.socket('rep');
  
-sock.bindSync('tcp://104.222.96.51:35000');
-console.log('Server bound to port 35000');
+sock.bindSync('tcp://104.222.96.51:33333');
+console.log('Server bound to port 33333');
  
 sock.on('message', function(msg){
   var message = msg.toString();
