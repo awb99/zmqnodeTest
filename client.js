f1feb2393d2482d2490a245aa9427c2d654b4b55
awb99 <awb99@gmail.com>
2016-01-27 16:18:32 +0100

    client does only one request

--- client.js
+++ client.js
@@ -5,12 +5,11 @@ var zmq = require('zmq')
 sock.connect('tcp://104.222.96.51:33333');
 console.log('Worker connected to port 33333');
 
-setInterval(function(){
-  console.log('requesting data');
-  sock.send('calendar');
-}, 5000);
-
- 
 sock.on('message', function(msg){
   console.log('reply data received: %s', msg.toString());
+  sock.close();
 });
+
+console.log('requesting data');
+sock.send('calendar');
+
