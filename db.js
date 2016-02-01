64b940a9f36b3c61713f5d9b609f2257f524ba9f
awb99 <awb99@gmail.com>
2016-02-01 17:35:40 +0100

    machine list

--- db.js
+++ db.js
@@ -15,14 +15,14 @@ var zmq = require('zmq'),
 console.log('Connecting to ZMQ order server at ' + appConfig.server.orderUrl);
 sock.connect(appConfig.server.orderUrl);
 
-var loadMachines = function(machine, callback) {
+var loadMachines = function(callback) {
   console.log('requesting machines list from zmq');
   sock.send('machines');
   // emitter.addListener(event, listener)is an alias for emitter.on(event, listener).
   //  You can use emitter.removeListener(event, listener)to remove a listener.
   // Or use emitter.once (event, listener) to install a listener "one use
   //sock.on('message', function(msg) {
-  sock.once('message', function(msg) {
+  sock.once( 'message', function(msg) {
     var json = msg.toString()
     console.log('machines list received from ZMQ.');
     callback(json);
